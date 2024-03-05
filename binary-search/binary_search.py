0b8c0f01894ba209bbcd2602da756ad01b92005d
nurgalive <azat.nur@gmail.com>
2024-03-05 10:28:36 +0100

    Added binary search implementation

---
+++ binary-search/binary_search.py
@@ -0,0 +1,20 @@
+# Grokking algos, page 9
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+def binary_search(arr: list, item: int) -> int: 
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+print(binary_search(arr, 17))
