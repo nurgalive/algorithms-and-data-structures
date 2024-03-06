8fec5f7be3a2acca80af72decaac789f73c03ee9
nurgalive <azat.nur@gmail.com>
2024-03-06 18:15:24 +0100

    Minor changes

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,6 +1,5 @@
 # Grokking algos, page 9
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# Complexity: O(log n)
 
 def binary_search(arr: list, item: int) -> int: 
     low = 0
@@ -17,4 +16,5 @@ def binary_search(arr: list, item: int) -> int:
             low = mid + 1
     return -1
 
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 print(binary_search(arr, 17))
