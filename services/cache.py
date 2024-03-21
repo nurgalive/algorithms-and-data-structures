57a1aa9cb43d7a9860f362d82d78a6b04dfd2cc8
nurgalive <azat.nur@gmail.com>
2024-03-21 16:21:47 +0100

    Imporved cache and add usage example

--- services/cache.py
+++ services/cache.py
@@ -1,6 +1,7 @@
 # How to implement the cache of size 10?
 # How you invalidate the cache?
 
+import time
 
 # TODO: make the cache estimate the size of the stored objects
 class Cache:
@@ -14,12 +15,22 @@ class Cache:
         self.cache_usage: dict[str, int] = {}
         print(f"Created cache with size: {self.cache_size}")
 
-    def get_from_cache(self, key: str) -> str:
-        self.cache_usage[key] = self.cache_usage.get(key, 0) + 1
-        print(f"Cache usage: {self.cache_usage}")
-        return self.cache.get(key, "No value saved in cache")
+    def get(self, key: str) -> int:
+        """
+        Get data from cache, if exists.
+        """
+        val = self.cache.get(key, False)
+        if val:
+            self.cache_usage[key] = self.cache_usage.get(key, 0) + 1
+            print(f"Cache usage: {self.cache_usage}")
+            return val
+        else:
+            return False
 
-    def add_to_cache(self, key, value):
+    def put(self, key, value):
+        """
+        Put data for storing in cache.
+        """
         if len(self.cache_usage) == self.cache_size:
             print("Cache is full.")
             self.cache_usage = dict(sorted(self.cache_usage.items(), key=lambda x: x[1], reverse=True))
@@ -31,12 +42,60 @@ class Cache:
         print(f"Cache: {self.cache}")
         print(f"Cache usage: {self.cache_usage}")
 
+def slow_read_from_disk(key: str) -> int:
+    """
+    Emulation of the slow reading from a disk.
+    """
+    data_on_disc = {
+        "table": 3,
+        "spoon": 1,
+        "chair": 2,
+        "pen": 5
+    }
+    time.sleep(2)
+
+    return data_on_disc.get(key, False)
+
+def get_data(key: str, cache: Cache) -> int:
+    """
+    Returns data from disk or from cache, is exists.
+    """
+    t = time.perf_counter()
+    val = cache.get(key)
+    if val:
+        print(f"Elapsed time cache: {time.perf_counter() - t:.4f}")
+        return val
+    else:
+        val = slow_read_from_disk(key)
+        if val:
+            cache.put(key, val)
+            print(f"Elapsed time disk: {time.perf_counter() - t:.4f}")
+            return val
+        else:
+            return False
 
 
 cache = Cache(3)
-print(cache.get_from_cache("table"))
-cache.add_to_cache("table", 3)
-print(cache.get_from_cache("table"))
-cache.add_to_cache("spoon", 1)
-cache.add_to_cache("chair", 2)
-cache.add_to_cache("pen", 5)
+### simple example
+# t = time.perf_counter()
+
+# print(get_data("table", cache))
+# print(get_data("table", cache))
+# print(get_data("spoon", cache))
+# print(get_data("chair", cache))
+# print(get_data("pen", cache))
+
+# print(f"Full elapsed time: {time.perf_counter() - t:.4f}")
+
+### user interaction example with the feeling of performance
+
+while True:
+    user_input = input("Type your object...\n")
+    if user_input == "exit":
+        break
+    else:
+        val = get_data(user_input, cache)
+        if val:
+            print(val)
+        else:
+            print("No such value!")
