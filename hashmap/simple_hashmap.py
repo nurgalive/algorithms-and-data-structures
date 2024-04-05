646722fbef77ed80483d3fd5dd0cfbb578ec29dc
nurgalive <azat.nur@gmail.com>
2024-04-05 16:41:56 +0200

    Finished for now implementation of the simple_hashmap with simple linked_list

--- hashmap/simple_hashmap.py
+++ hashmap/simple_hashmap.py
@@ -52,10 +52,20 @@ class LinkedList():
         node = self.head
         nodes = []
         while node is not None:
-            nodes.append(node.data)
+            nodes.append(str(node.data))
             node = node.next
-        nodes.append("None")
+        # nodes.append("None")
         return " -> ".join(nodes)
+    
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+    
+    # TODO: Implement deletion of the a node
+    # def delete_node(self):
+        
 
 # 1. What happens, if there is already a value in the index? -> Collision resolution. Store values as linked list pairs
 # 2. What if we need to grow the array? -> calculate load factor and resizing
@@ -66,14 +76,25 @@ class Hashmap:
     Simple hash map. As a hash function used sum of the prime numbers.
     Where each prime number calculated based on the position of the letter.
     a - has first prime number, b - second, z - 26th prime number.
+    Data is stored in the array. Where every cell of the array called bucket.
+    Every bucket is represented as a Linked List.
     """
     letter_dict = dict(zip(ascii_lowercase, get_prime_numbers1(26)))
+    load_factor_threshold = 0.7
 
     def __init__(self, size):
-        self.array = [None for i in range(size)]  # initial size 10
+        self.array: list[None] | list[LinkedList] = [None for i in range(size)]  # initial size 10
         print("Created hashmap with size:", len(self.array))
     def __len__(self):
         return len(self.array)
+    
+    def __repr__(self) -> str:
+        result = []
+        for bucket in self.array:
+            if bucket is not None:
+                for node in bucket:
+                    result.append(f"{node.data[0]}: {node.data[1]}")
+        return " ".join(result)
 
     def calculate_index(self, key) -> int:
         """
@@ -85,63 +106,121 @@ class Hashmap:
         for letter in key:
             letter_sum += self.letter_dict[letter]
 
-        index = letter_sum % 10 - 1  # -1 since the array has 0-based indexing
+        index = letter_sum % self.size  # -1 since the array has 0-based indexing
         print("Calculated index:", index, "; for the key:", key)
         return index
 
     def put(self, key: str, val: int) -> None:
         """
-        Store keys and values in 
+        Store keys and values in hashmap.
         """
+        print("Put value:", val, "; for the key:", key)
+        current_load_factor = self.load_factor
+        print("current_load_factor", current_load_factor)
+        if current_load_factor >= self.load_factor_threshold:
+            print("load_factor", current_load_factor, "is bigger than", self.load_factor_threshold)
+            self.resize()
         index = self.calculate_index(key)
-        if self.array[index]:
+        if self.array[index]: # if there is LinkedList in the bucket, add to it
             self.array[index].add_first((key, val))
-        else:
+        else: # if there is no LinkedList in the bucker, create it
             self.array[index] = LinkedList()
             self.array[index].add_first((key, val))
         
-        print("Put value:", val, "; for the key:", key)
         print("Data array:", self.array)
 
     def get(self, key: str) -> int:
-        value = self.array[self.calculate_index(key)]
-        if value:
-            return value
-        raise KeyError(key)
+        bucket = self.array[self.calculate_index(key)]  # bucket == LinkedList
+        if bucket is None:  # bucket not initialized
+            raise KeyError(key)
+        for node in bucket:  # iterate over bucket (linked list) to search for value
+            # node(key, value)
+            if node.data[0] == key:
+                return node.data[1]
+        raise KeyError(key)  # key not found in bucket (linked list)
 
-    def load_factor(self):
-        pass
 
     def resize(self):
-        pass
+        """
+        This method called, if the load_factor_threshold is reached and
+        the array for storing data have to be resized and elements have to be
+        copied over and rehashed.
+        """
+        print("Resizing the hashmap's underlying array...")
+        old_array = self.array
+        new_size = len(old_array) * 2
+        print("new size is:", new_size)
+        self.array = [None for i in range(new_size)]
+        for bucket in old_array:
+            if bucket is not None:
+                for node in bucket:  # bucket is Linked List, and node is Node.
+                    self.put(node.data[0], node.data[1])
+
+    @property
+    def load_factor(self) -> float:
+        # occupied / overall_size 
+        # 3 / 10 
+        occupied = sum([1 for val in self.array if val is not None])
+        load_factor = round(occupied / len(self.array), 2)
+        return load_factor
+
+    
+    @property
+    def size(self):
+        return len(self.array)
+    
+    # TODO: Implement the deletion of the key-value pair.
+    # def delete(self, key):
+
+if __name__ == "__main__":
 
 
-# hashmap = Hashmap(10)
-# val_iterator = 1
-# # random_words = get_random_words(3)
-# random_words = ["withdrawal", "registration", "guidelines"]
-# for word in random_words:
-#     hashmap.put(word, val_iterator)
-#     val_iterator += 1
+    # hashmap = Hashmap(3)
+    # # hashmap.put("withdrawal", 1)
 
-# for word in random_words:
-#     print("For word:", word, "we got value:", hashmap.get(word))
+    # val_iterator = 1
+    # # random_words = get_random_words(3)
+    # random_words = ["withdrawal", "registration", "guidelines"]
+    # for word in random_words:
+    #     hashmap.put(word, val_iterator)
+    #     val_iterator += 1
 
-# hashmap.get("aaa")
+    # hashmap.put("bro", 4)
 
-### testing
-# letter_dict = dict(zip(ascii_lowercase, get_prime_numbers1(26)))
+    # for word in random_words:
+    #     print("For word:", word, "we got value:", hashmap.get(word))
 
-# word = "bag"
-# w_sum = 0
-# for w in word:
-#     w_sum += letter_dict[w]
+    # hashmap.get("aaa")
 
-# res = w_sum % 10
-# print(res)
+    ### testing
+    # letter_dict = dict(zip(ascii_lowercase, get_prime_numbers1(26)))
 
-llist = LinkedList()
-llist.add_first("a")
-llist.add_first("b")
-llist.add_first("c")
-print(llist)
+    # word = "bag"
+    # w_sum = 0
+    # for w in word:
+    #     w_sum += letter_dict[w]
+
+    # res = w_sum % 10
+    # print(res)
+
+    ## Testing the linked list
+    # llist = LinkedList()
+    # llist.add_first(("a", "b"))
+    # llist.add_first("b")
+    # llist.add_first("c")
+    # print(llist)
+    # for val in llist:
+    #     print(val.data)
+
+    ## testing collisions with the linked list
+    hashmap = Hashmap(5)
+
+    val_iterator = 1
+    # random_words = get_random_words(3)
+    random_words = ["withdrawal", "withdrwaal", "iwthdrawal"]
+    for word in random_words:
+        hashmap.put(word, val_iterator)
+        val_iterator += 1
+
+    for word in random_words:
+        print("For word:", word, "we got value:", hashmap.get(word))
