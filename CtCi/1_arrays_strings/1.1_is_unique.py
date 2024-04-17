fcedc7e2a12405e283f0456a22b223b56b1229e0
nurgalive <azat.nur@gmail.com>
2024-04-17 21:09:17 +0200

    Added additional description

--- CtCi/1_arrays_strings/1.1_is_unique.py
+++ CtCi/1_arrays_strings/1.1_is_unique.py
@@ -1,3 +1,4 @@
+# Cracking the coding interview. Exercise 1.1. Page 90.
 # Is Unique: Implement an algorithm to determine if a string has all unique characters. 
 # What if you cannot use additional data structures?
 
--- CtCi/1_arrays_strings/1.2_check_permutation.py
+++ CtCi/1_arrays_strings/1.2_check_permutation.py
@@ -1,6 +1,11 @@
+# Cracking the coding interview. Exercise 1.2. Page 90.
+# Check Permutation: Given two strings, write a method to decide 
+# if one is a permutation of the other.
+
 str1 = "abc"
 str2 = "bca"
 
+# brute force solution
 def check_permutatoin(str1, str2):
 	for s1 in str1:
 		for s2 in str2:
@@ -9,5 +14,8 @@ def check_permutatoin(str1, str2):
 		else:
 			return False
 	return True
+
+# use the array of ascii symbols 128, like in the previous task
+# def check..
 	
 print(check_permutatoin(str1, str2))
