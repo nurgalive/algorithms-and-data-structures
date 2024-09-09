6914b47bfc576ff7e556ad18718d7f0f0c489ba5
nurgalive <github@nurgalive.com>
2024-09-09 17:01:54 +0200

    Added more details

--- recursion/fib.py
+++ recursion/fib.py
@@ -1,10 +1,28 @@
-# Cracking the coding interview. Example 14. Page 53.
+"""
+Cracking the coding interview. Example 14. Page 53.
+
+https://en.wikipedia.org/wiki/Fibonacci_sequence
+
+F(n): 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ....
+n:    0  1  2  3  4  5  6  7   8   9   10  11  12
+
+Next number is sum of two previous numbers.
+Common example of applying a recursive algo.
+
+"""
+
 
 def allFib(n: int):
+    """
+    Prints fibonacci numbers from 0 to Nth.
+    """
     for i in range(0, n):
         print(f"{i}: {fib(i)}")
     
 def fib(n: int) -> int:
+    """
+    Searches for the Nth fibonacci number.
+    """
     if n <= 0: return 0
     elif n == 1: return 1
     return fib(n -1) + fib(n - 2)
--- recursion/fib2.py
+++ recursion/fib2.py
@@ -1,16 +1,21 @@
-# Cracking the coding interview. Example 15. Page 53.
+"""
+Cracking the coding interview. Example 15. Page 53.
+
+Improved version of searching for Nth fibonacci number function using cache.
+For details about Fibonacci numbers loot in fib.py
+"""
 
 def allFib(n: int):
-    memo = [0 for i in range(0, n)]
+    memo = [0 for i in range(0, n)]  # init the cache
     for i in range(0, n):
         print(f"{i}: {fib(i, memo)}")
 
 def fib(n: int, memo: list) -> int:
     if n <= 0: return 0
     elif n == 1: return 1
-    elif memo[n] > 0: return memo[n]
+    elif memo[n] > 0: return memo[n]  # if there is a value in the cache, use it
     else:
-        memo[n] = fib(n -1, memo) + fib(n -2, memo)
+        memo[n] = fib(n -1, memo) + fib(n -2, memo)  # else calculate value and put in cache
         return memo[n]
     
 
