955ee3bc0b1ea4c9e6fad6b79b781b2b001c3de0
nurgalive <github@nurgalive.com>
2024-09-11 12:04:17 +0200

    Added more sultions, tests and java version for later

---
+++ CtCi/c1_arrays_strings/q1_04_palindrome/Palindrome.java
@@ -0,0 +1,51 @@
+package q1_04_palindrome;
+
+/*
+ * Solution from the author.
+ * https://github.com/careercup/CtCI-6th-Edition/blob/master/Java/Ch%2001.%20Arrays%20and%20Strings/Q1_04_Palindrome_Permutation/QuestionC.java
+ * 
+ * Left it here to solve it later in python.
+ * 
+ */
+
+public class Palindrome {
+	/* Toggle the ith bit in the integer. */
+	public static int toggle(int bitVector, int index) {
+		if (index < 0) return bitVector;
+		
+		int mask = 1 << index;
+		if ((bitVector & mask) == 0) {
+			bitVector |= mask;
+		} else {
+			bitVector &= ~mask;
+		}
+		return bitVector;
+	}
+	
+	/* Create bit vector for string. For each letter with value i,
+	 * toggle the ith bit. */
+	public static int createBitVector(String phrase) {
+		int bitVector = 0;
+		for (char c : phrase.toCharArray()) {
+			int x = Common.getCharNumber(c);
+			bitVector = toggle(bitVector, x);
+		}
+		return bitVector;
+	}
+	
+	/* Check that at most one bit is set by subtracting one from the 
+	 * integer and ANDing it with the original integer. */
+	public static boolean checkAtMostOneBitSet(int bitVector) {
+		return (bitVector & (bitVector - 1)) == 0;
+	}
+	
+	public static boolean isPermutationOfPalindrome(String phrase) {
+		int bitVector = createBitVector(phrase);
+		return checkAtMostOneBitSet(bitVector);
+	}
+	
+	public static void main(String[] args) {
+		String pali = "Rats live on no evil star";
+		System.out.println(isPermutationOfPalindrome(pali));
+	}
+}
--- CtCi/c1_arrays_strings/q1_04_palindrome/palindrome.py
+++ CtCi/c1_arrays_strings/q1_04_palindrome/palindrome.py
@@ -15,7 +15,8 @@ Brut-force solution - s!
 
 What palindrome is?
 - all letters divided by two
-- one letter is unique
+- one or zero letter is unique
+- example: anna, ana, an na
 
 Count frequencies of the letters
 - min O(s)
@@ -26,36 +27,127 @@ inefficient. [Yes, use frequencies]
 #121.1.4 What characteristics would a string that is a permutation of a palindrome have? [They have even number of letter, except one]
 #134.1.4 Have you tried a hash table? You should be able to get this down to 0(N) time. [Yes]
 #136.1.4 Can you reduce the space usage by using a bit vector?
+
+Solution from the author:
+https://github.com/careercup/CtCI-6th-Edition/tree/master/Java/Ch%2001.%20Arrays%20and%20Strings/Q1_04_Palindrome_Permutation
+
+
+Python solution:
+https://github.com/careercup/CtCI-6th-Edition-Python/blob/e6bc732588601d0a98e5b1bc44d83644b910978d/Chapter1/4_Palindrome%20Permutation/PalindromePermutation.py
+
+odd - нечетный
+even - четный
 """
+
+import unittest
 
 def palindrome_hashmap(input: str):
     """
-    O(s)
-    Int can be used to store . English letters < 32
-    Count frequencies using hash map.
+    O(s) - time and space.
+    Simplest solution. Treats all letters as lowercase. Only checks for 
+    Int can be used to store . English letters (26) < 32
+    Count frequencies using hash map. Uses a bit more memory, than array. But
+    does not require ord() conversion to find id.
+
+    This approach uses two loops. But in the loop less operations.
     """
 
     freq = dict()
 
-    for s in input:  # O(s)
-        if s == " ": continue
-        freq[s] =  freq.get(s, 0) + 1
+    non_letter_chars = {" ", ","}
 
-    not_even = False
+    for s in input:  # O(s)
+        if s in non_letter_chars: continue
+        freq[s.lower()] =  freq.get(s.lower(), 0) + 1
+
+    odd_found = False
     for val in freq.values():  # O(1)
         if val % 2 == 1:
-            if not_even:
-                return False
-            else:
-                not_even = True
+            if odd_found:  # if found again
+                return False  # return false (not a palindrome)
+            else:  # found a first not even nuber
+                odd_found = True
     else:
         return True
 
-def palindrome_bitvector(input: str):
+def palindrome_array(input: str) -> bool:
     """
-    Check is_unique.py for details.
+    O(s) - time and space.
+    Uses the same idea of applying ord() from the is_unique.py instead of
+    using hashmap. Because of the a little bit more memory efficient.
+    Uses the ord() conversion of the letters. Checks the result by every iteration of the
+    loop. Only one loop, but does more operations per iteration.
     """
-    pass
+    #                                 122   -    97
+    counter = [0 for _ in range(0, ord('z') - ord('a') + 1)]  # initializing the array with lowercase letters a-z
+
+    odd_count = 0
+    for s in input:
+        char_id = get_char_id(s)
+        if char_id != -1:
+            counter[char_id] += 1
+            if counter[char_id] % 2 == 1:
+                odd_count += 1
+            else:
+                odd_count -= 1
+                
+    
+    else:
+        return odd_count <= 1
+
+
+def get_char_id(char: str) -> int:
+
+    # convert 'a-z' to a num '0-26'
+    if ord("a") <= ord(char) <= ord("z"):
+        return ord(char) - ord("a")
+    else:
+        # convert 'A-Z' to a num '0-26'
+        if ord("A") <= ord(char) <= ord("Z"):
+            return ord(char) - ord("A")
+    return -1  # if it is not a letter
+
+
+
+# def palindrome_bitvector(input: str):
+#     """
+#     Check is_unique.py for details.
+#     """
+#     pass
+
+
+class TestPalindrome(unittest.TestCase):
+    def test_palindrome_simple(self):
+        self.assertEqual(palindrome_hashmap("taco cat"), True)
+        self.assertEqual(palindrome_hashmap("Rats live on no evil star"), True)
+        self.assertEqual(palindrome_hashmap("Able was I ere I saw Elba"), True)
+        self.assertEqual(palindrome_hashmap("Lleve"), True)
+        self.assertEqual(palindrome_hashmap("Tacotac"), True)
+        self.assertEqual(palindrome_hashmap("asda"), False)
+        self.assertEqual(palindrome_hashmap("A man, a plan, a canal, panama"), True)
+
+    def test_palindrome_array(self):
+        self.assertEqual(palindrome_array("taco cat"), True)
+        self.assertEqual(palindrome_array("Rats live on no evil star"), True)
+        self.assertEqual(palindrome_array("Able was I ere I saw Elba"), True)
+        self.assertEqual(palindrome_array("Lleve"), True)
+        self.assertEqual(palindrome_array("Tacotac"), True)
+        self.assertEqual(palindrome_array("asda"), False)
+        self.assertEqual(palindrome_array("A man, a plan, a canal, panama"), True)
+        
+
+class TestGetCharId(unittest.TestCase):
+    def test_get_char_id(self):
+        self.assertEqual(get_char_id("a"), 0)
+        self.assertEqual(get_char_id("b"), 1)
+        self.assertEqual(get_char_id("z"), 25)
+        self.assertEqual(get_char_id("A"), 0)
+        self.assertEqual(get_char_id("B"), 1)
+        self.assertEqual(get_char_id("Z"), 25)
+        self.assertEqual(get_char_id("@"), -1)
+        self.assertEqual(get_char_id(" "), -1)
+        self.assertEqual(get_char_id(","), -1)
 
 if __name__ == "__main__":
-    print(palindrome_hashmap("taco cat"))
+    # print(palindrome_array("taco cat"))
+    unittest.main()
