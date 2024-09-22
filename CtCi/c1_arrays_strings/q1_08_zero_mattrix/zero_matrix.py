a0af7d128161b47ce93c6e113066fcdf0fca28fb
nurgalive <github@nurgalive.com>
2024-09-22 22:29:36 +0200

    Added file

---
+++ CtCi/c1_arrays_strings/q1_08_zero_mattrix/zero_matrix.py
@@ -0,0 +1,15 @@
+"""
+Cracking the coding interview. Exercise 1.4. Page 91.
+
+Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
+column are set to 0.
+Hints:#17, #74, #702
+
+
+"""
+
+def zero_matrix(matrix: list[list[int]]) -> None:
+    pass
+
+if __name__ == "__main__":
+    zero_matrix(list())
