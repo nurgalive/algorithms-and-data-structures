7a1380b6951ac59f7a58788265fbc87a143752ef
nurgalive <github@nurgalive.com>
2024-09-23 22:02:12 +0200

    Added more references

--- CtCi/c1_arrays_strings/q1_07_rotate_matrix/rotate_matrix.py
+++ CtCi/c1_arrays_strings/q1_07_rotate_matrix/rotate_matrix.py
@@ -10,11 +10,32 @@ Hints: #51, #100
 asked to swap the values in two arrays, could you do this? Can you then extend it to four
 arrays?
 
+Leetcode: https://leetcode.com/problems/rotate-image/
+
 This problem is very hard to me.
 Those many indexes drives me crazy.
 
-Amazing explanation.
+Amazing explanation using rotation. Similar to the book author solution:
 https://www.youtube.com/watch?v=fMSJSS7eO1w
+
+Solution using transposing and reflecting the matrix:
+https://www.youtube.com/watch?v=-jhbxNJijyE
+
+Explaining how the way of thinking with matrixes can looks like:
+https://www.youtube.com/watch?v=Z0R2u6gd3GU
+
+Examples:
+Python
+https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/chapter_01/p07_rotate_matrix.py
+
+Java
+https://github.com/careercup/CtCI-6th-Edition/blob/master/Java/Ch%2001.%20Arrays%20and%20Strings/Q1_07_Rotate_Matrix/Question.java
+
+
+
+All the solutions are:
+- Time complexity: O(N^2). Since we need to touch all the elements of n-sized square matrix.
+- Space complexity: O(1). We do a in-place updates.
 
 """
 class Question:
@@ -60,26 +81,34 @@ class Question:
 
         return True
 
-    def print_matrix(self):
-        for i in range(len(self.matrix)):
-            if len(self.matrix) != len(self.matrix[i]):
-                return False
-        height = len(self.matrix)
-        width = len(self.matrix[0])
+def print_matrix(matrix):
+    for i in range(len(matrix)):
+        if len(matrix) != len(matrix[i]):
+            return False
+    height = len(matrix)
+    width = len(matrix[0])
 
-        for h in range(height):
-            print("")
-            for w in range(width):
-                print(self.matrix[h][w], end="\t")
-        
+    for h in range(height):
         print("")
-        return True
+        for w in range(width):
+            print(matrix[h][w], end="\t")
     
-
+    print("")
+    return True
+    
+def rotate_matrix_pythonic_alternate(matrix):
+    """rotates a matrix 90 degrees clockwise"""
+    return [list(reversed(row)) for row in zip(*matrix)]
 
 if __name__ == "__main__":
     input = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print_matrix(input)
+
+    # solution 1
     q = Question(input)
-    q.print_matrix()
+    print_matrix(q.matrix)
     q.rotate_matrix()
-    q.print_matrix()
+    print_matrix(q.matrix)
+
+    # solution 2
+    print_matrix(rotate_matrix_pythonic_alternate(input))
