c7c1f180bd294b4ad798beaa4f8c3e2a2e2255b4
nurgalive <azat.nur@gmail.com>
2024-11-18 23:52:26 +0100

    Fixed typos and formatting

--- leetcode/prefix_sum/prefix_sum.py
+++ leetcode/prefix_sum/prefix_sum.py
@@ -8,7 +8,7 @@ With negative numbers
 Array               = [1, 2, -3, 5]
 Prefix sum array    = [0, 1, 3, 0, 5]
 
-Formula to get the range sum of the subarray depends of the approach/
+Formula to get the range sum of the subarray depends of the approach.
 
 """
 
@@ -17,20 +17,22 @@ def build_prefix_sum_1_based(arr):
     n = len(arr)
     prefix_sum = [0] * n
     prefix_sum[0] = arr[0]
-    
+
     for i in range(1, n):
         prefix_sum[i] = prefix_sum[i - 1] + arr[i]
-    
+
     return prefix_sum
+
 
 def build_prefix_sum_0_based(arr):
     n = len(arr)
     prefix_sum = [0] * (n + 1)
-    
+
     for i in range(0, n):
         prefix_sum[i + 1] = prefix_sum[i] + arr[i]
-    
+
     return prefix_sum
+
 
 def range_sum_1_based(prefix_sum, l, r):
     """
@@ -43,8 +45,9 @@ def range_sum_1_based(prefix_sum, l, r):
         return prefix_sum[r]
     else:
         return prefix_sum[r] - prefix_sum[l - 1]
-    
-def range_sum2(prefix_sum, l, r):
+
+
+def range_sum_0_based(prefix_sum, l, r):
     """
     l - left pointer
     r - right pointer
@@ -52,6 +55,7 @@ def range_sum2(prefix_sum, l, r):
     Formula for the range queries: prefix_sum[r + 1] - prefix_sum[l]
     """
     return prefix_sum[r + 1] - prefix_sum[l]
+
 
 def subarraySum(nums: list[int], k: int) -> int:
     cumsum = 0
@@ -66,6 +70,7 @@ def subarraySum(nums: list[int], k: int) -> int:
         cumsum_map[cumsum] = cumsum_map.get(cumsum, 0) + 1
         print("cumsum_map", cumsum_map)
     return res
+
 
 # arr = [1, 2, 3, -1, -2, -3]
 arr = [1, -1, 1, -1, 1, 1]
@@ -84,6 +89,3 @@ print(subarraySum(arr, k))
 # print("Prefix Sum Array2:", P2)
 # print("Sum from index 1 to 3:", range_sum(P, 0, 3))  # Output: 6
 # print("Sum from index 1 to 3:", range_sum2(P2, 0, 3))  # Output: 6
-
-
-
